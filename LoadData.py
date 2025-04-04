c810b818e5151c2bb52a50c756c2115d2596ac4c
Christine Wong <55353492+Lillliant@users.noreply.github.com>
2025-04-04 11:22:39 -0400

    Update LoadData.py to load t25i10d10k and retail

--- LoadData.py
+++ LoadData.py
@@ -1,95 +1,42 @@
-from models.DataSet import Trajectory, SeqDataSet
+from models.DataSet import Itemset, ItemDataSet
 import pickle
 
+"""
+Structure
+Dataset -> DataSet.ItemDataSet
+            # The list of items in the domain
+            self.points = points 
+            # The list of transactions, each transaction is a list of items in DataSet.Itemset format
+            self.record = [] 
+Transaction -> DataSet.Itemset
+            # The list of unique items in the transaction
+            self.data = set(data) 
+            # The length of the transaction (i.e., number of elements)
+            self.data_length = len(data) 
+"""
 
-def loadMsnbc(minLength=3,maxLength=None):
-    msnbc_path = 'data/msnbc/msnbc990928.seq'
-    with open(msnbc_path) as fp:
-        lines = fp.readlines()
-        line_num = len(lines)
-    dataset = SeqDataSet(list(range(17)))
-    for i in range(7,line_num):
-        line = lines[i]
-        line_split = line.split()
+def loadDataSet(data_path: str, name: str, output_dir: str):
+    records = []
+    items = set()
+    print("Loading data...")
+    with open(data_path, 'r') as f:
+        lines = f.readlines()
+        for line in lines:
+            transaction = list(map(int, line.strip().split()))
+            items.update(transaction)
+            records.append(transaction)
+    print("Converting to FedFPM format...")
+    dataset = ItemDataSet(list(items))
+    for record in records:
+        transaction = Itemset(record)
+        dataset.add_line(transaction)
+    print(f"Saving data to {output_dir}/{name}.pickle...")
+    with open(f'{output_dir}/{name}.pickle', 'wb') as f:
+        pickle.dump(dataset, f)
+    print("Data saved successfully.")
 
-        for i in range(len(line_split)):
-            line_split[i] = int(line_split[i]) - 1 # locations starts form 0
-        
-        if minLength is not None and len(line_split) < minLength:
-            continue
-
-        if maxLength is not None and len(line_split) > maxLength:
-            continue
-
-        traj = Trajectory(line_split)
-        dataset.add_line(traj)
-
-    with open("./data/msnbc.pickle",'wb') as fp:
-        pickle.dump(dataset,fp)
-    
-    return dataset
-
-def loadMsnbc_trimmed(requiredlocations,minLength=3,maxLength=None):
-    msnbc_path = 'data/msnbc/msnbc990928.seq'
-    with open(msnbc_path) as fp:
-        lines = fp.readlines()
-        line_num = len(lines)
-    dataset = SeqDataSet(list(range(requiredlocations)))
-
-    location_count = {}
-    for i in range(7,line_num):
-        line = lines[i]
-        line_split = line.split()
-
-        for i in range(len(line_split)):
-            line_split[i] = int(line_split[i]) # locations starts form 0
-        
-        if minLength is not None and len(line_split) < minLength:
-            continue
-
-        if maxLength is not None and len(line_split) > maxLength:
-            continue
-
-        for k in line_split:
-            if k not in location_count.keys():
-                location_count[k] = 0
-            location_count[k] += 1
-        
-    locationsort = sorted(location_count.items(),key=lambda x: x[1],reverse=True)
-    locationdict = {}
-    for i in range(requiredlocations):
-        locationdict[locationsort[i][0]] = i
-    print(locationdict)
-
-    for i in range(7,line_num):
-        line = lines[i]
-        line_split = line.split()
-
-        for i in range(len(line_split)):
-            line_split[i] = int(line_split[i]) # locations starts form 0
-        
-        if minLength is not None and len(line_split) < minLength:
-            continue
-
-        if maxLength is not None and len(line_split) > maxLength:
-            continue
-
-        trimmedtraj = []
-        for k in line_split:
-            if k not in locationdict.keys():
-                continue
-            trimmedtraj.append(locationdict[k])
-        
-        traj = Trajectory(trimmedtraj)
-        dataset.add_line(traj)
-
-    filepath = "./data/msnbc" + str(requiredlocations) + '.pickle'
-    with open(filepath,'wb') as fp:
-        pickle.dump(dataset,fp)
-    
-    return dataset
-
-
-if __name__ == '__main__':
-    #ds = loadMsnbc()
-    loadMsnbc_trimmed(requiredlocations=14)
+if __name__ == "__main__":
+    dataset = 't25i10d10k' # 't25i10d10k' or 'retail'
+    data_dir = f'./raw/{dataset}.txt'
+    output_dir = './data'
+    loadDataSet(data_dir, dataset, output_dir)
